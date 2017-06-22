9ea151aecb99164d8546c61a471063a00fed461f
刘海峰 <lhfzxc@gmail.com>
2017-06-22 15:20:28 +0800

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Valentino Rossi on 2017/6/22.
+//
+
+#include <iostream>
+#include <pthread.h>
+#include "TOTP/library.h"
+
+using namespace std;
+
+void *callbacksss(void *pt) {
+
+    printf("callback");
+
+
+}
+
+
+int main() {
+
+
+    hello();
+
+
+    return 0;
+}
+
